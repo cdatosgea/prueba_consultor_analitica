c03567357afaf95529be2fa4931a5e916d6cf22c
Andresv0701 <analista.bases1@riskgc.com>
2024-08-09 07:14:42 -0500

    se agrega grafico de calificaciones

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from w_scraping import scrape_books
 import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
 
 st.title("Libros a la venta")
 
@@ -36,3 +38,23 @@ else:
 st.write('DataFrame filtrado:')
 st.dataframe(df_filtrado)
 ###
+
+# Título de la aplicación
+st.title('distribución de calificaciones')
+
+categorias = df['calification']
+contador = Counter(categorias)
+labels = list(contador.keys())
+cantidades = list(contador.values())
+
+# Calcular porcentajes
+total = sum(cantidades)
+porcentajes = [(cantidad / total) * 100 for cantidad in cantidades]
+
+# Crear gráfico circular
+fig, ax = plt.subplots()
+ax.pie(porcentajes, labels=labels, autopct='%1.1f%%', startangle=90)
+ax.axis('equal')  # Para asegurar que el gráfico sea un círculo
+
+# Mostrar gráfico en Streamlit
+st.pyplot(fig)
